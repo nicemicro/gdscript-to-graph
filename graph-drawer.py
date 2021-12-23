2e78d2cc880d62d369967a95dfec21e5a7391566
nicemicro <63175285+nicemicro@users.noreply.github.com>
2021-12-23 23:04:54 +0900

    initial working prototype

---
+++ graph-drawer.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 23 15:11:13 2021
+
+@author: nicemicro
+"""
+
+import os
+import enum
+
+class place(enum.Enum):
+    universal = enum.auto()
+    server = enum.auto()
+    client = enum.auto()
+
+def find_files(path):
+    filelist = []
+    for dirpath, dirs, files in os.walk(path):  
+        for filename in files:
+            if filename[-3:] == ".gd":
+                filelist.append({"dir": dirpath, "file": filename})
+    return filelist
+
+def process_file(path):
+    functions = {}
+    current_type = place.universal
+    current_func = ""
+    ftabs = 0
+    tabs = 0
+    prev_tabs = 0
+    comment = ""
+    
+    # This variable keeps track of the current nesting level
+    func_ctrl = []
+    
+    with open(path) as gdfile:
+        lines = gdfile.readlines()
+    for fline in lines:
+        line = fline.split("#")[0]
+        line = line.strip()
+        if len(fline.split("#")) > 1:
+            commentline = fline.split("#")[1]
+            if commentline and commentline[0] != " " and commentline[0] != "\t":
+                commentline = ""
+        else:
+            commentline = ""
+        commentline = commentline.strip()
+        tabs = len(fline) - len(fline.lstrip('\t'))
+
+        # checks whether this line is a deliniator for server / client functs
+        if (commentline[0:2] == "--" or commentline[0:3] == " --") and len(line) == 0:
+            if "Server" in commentline or "server" in commentline:
+                current_type = place.server
+            elif "Client" in commentline or "client" in commentline:
+                current_type = place.client
+            continue
+        
+        # checks whether this line is the start of a new function
+        functionstart = (line[0:4] == "func" or line[0:11] == "puppet func" or
+                         line[0:11] == "master func" or
+                         line[0:15] == "mastersync func" or
+                         line[0:15] == "puppetsync func")
+        if functionstart:
+            funcname = line.split("func ")[1]
+            funcname = funcname.split("(")[0]
+            current_func = funcname
+            functions[current_func] = {"type": current_type, "ctrl": []}
+            func_ctrl = [functions[current_func]["ctrl"]]
+            ftabs = tabs
+            comment = ""
+            continue
+        
+        # if the current tab depth is lower than the previous, we pop
+        if tabs < prev_tabs and len(func_ctrl) > 0:
+            for i in range(prev_tabs-tabs):
+                func_ctrl.pop()
+        
+        if len(commentline) > 0:
+            comment = comment + "\n" + commentline
+        
+        # ignore empty lines and ones that are not tabulated deeper than the
+        # last function declaration
+        if current_func != "" and line != "" and tabs > ftabs:
+            controlitem = (line[-1] == ":")
+            ignoreitem = (line[0:5] == "print" or line[0:6] == "assert")
+            if controlitem:
+                if comment == "":
+                    func_ctrl[-1].append([line])
+                else:
+                    func_ctrl[-1].append([comment[1:]])
+                func_ctrl.append(func_ctrl[-1][-1])
+                comment = ""
+            elif not ignoreitem:
+                if comment == "":
+                    func_ctrl[-1].append(line)
+                else:    
+                    func_ctrl[-1].append(comment[1:])
+                comment = ""
+            else:
+                comment = ""
+        prev_tabs = tabs
+    return functions
+
+def read_all(path):
+    filelist = find_files(path)
+    files = {}
+    for filename in filelist:
+        file = process_file(os.path.join(filename["dir"], filename["file"]))
+        files[filename["file"]] = file
+    return files
+
+def control_extract(operations, string, counter):
+    for num, op in enumerate(operations):
+        if num > 0:
+            string = string  + " | "
+        if isinstance(op, list):
+            string = string + "{"
+            string, counter = control_extract(op, string, counter)
+            string = string + "}"
+        else:
+            op = op.replace("\"", "\\\"")
+            op = op.replace("'", "\\'")
+            op = op.replace("{", "\\{")
+            op = op.replace("}", "\\}")
+            op = op.replace("<", "\\<")
+            op = op.replace(">", "\\>")
+            op = op.replace("\n", "\\n")
+            string = string + f"<f{counter}> {op} "
+            counter += 1
+    return string, counter
+
+def function_unit(function, fullname, functname):
+    record = ""
+    record, cnt = control_extract(function, record, 0)
+    print(f"            {fullname} [")
+    print(f"                label=\"<begin>{functname} | {record}\"")
+    print("                shape=\"record\"")
+    print("            ]")
+
+def file_subgraph(file, fname, servcl, serverclient):
+    fname_cut = fname.split(".")[0]
+    print(f"        subgraph cluster_{servcl}_{fname_cut} " + "{")
+    print(f"            label=\"{fname}\"; labeljust=\"l\";")
+    for function in file:
+        functype = file[function]["type"]
+        if functype == serverclient or functype == place.universal:
+            function_unit(file[function]["ctrl"],
+                          f"{servcl}_{fname_cut}_{function}",
+                          function)
+    print("        }")
+
+def sc_subgraph(files, servcl, serverclient):
+    subg_name = servcl.lower().replace(" ", "")
+    print(f"    subgraph cluster_{subg_name} " + "{")
+    print(f"        label=\"{servcl}\"; labeljust=\"l\";")
+    for filen in files:
+        file_subgraph(files[filen], filen, subg_name, serverclient)
+    print("    }")
+
+def make_graph(files):
+    print("digraph controlflow {")
+    print("    rankdir=\"LR\";")
+    sc_subgraph(files, "Client 1", place.client)
+    sc_subgraph(files, "Server", place.server)
+    sc_subgraph(files, "Client 2", place.client)
+    print("}")
+#%%
+make_graph(read_all("/home/nicemicro/bin/opensus/src/"))
+#a = process_file("/home/nicemicro/bin/opensus/src/autoloads/resources.gd")
